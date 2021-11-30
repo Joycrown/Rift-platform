dc0df56b8d47d5d2f41cbec7178ebd9fb97fb4d8
unknown <olaniyanayoade@ymail.com>
2021-11-30 09:20:45 +0100

    new push

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -65,11 +65,11 @@ function Home(){
                             <form action="https://link.us17.list-manage.com/subscribe/post?u=3d0c961e139280b56dff09d86&amp;id=ddbe36e928" method="post" class="row g-0">
                                 <div id="mc_embed_signup_scroll">
                                     <div className="wrapper wrapper-medium-12">
-                                        <input type="email" value = {formValues.EMAIL} onChange ={handleChange}className="form-control email-space email-space-medium-1 rounded-pill required email"  name="EMAIL" placeholder= "Your email" id="inputEmail4 mce-EMAIL" required/>
-                                        <button   type="submit" value="Subscribe" name="subscribe"  className="btn button email-btn email-btn-1-md rounded-pill clear" id ='join mc-embedded-subscribe'>Join waitlist now</button>
+                                        <input type="email" value = {formValues.EMAIL} onChange ={handleChange}className="form-control email-space email-space-medium-1 rounded-pill required email"  name="EMAIL" placeholder= "Your email" id="inputEmail4" required/>
+                                        <button   type="submit" value="Subscribe" name="subscribe"  className="btn button email-btn email-btn-1-md rounded-pill clear" id ='join'>Join waitlist now</button>
                                     </div>
                                 </div>
-                                <p className= 'h6 py-1 error'> {formErrors.EMAIL}</p>
+                               
                             </form>
                         </div>
                                                             
