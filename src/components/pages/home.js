86b7511c36dda3c394012d6fbf14c782b31e554a
unknown <olaniyanayoade@ymail.com>
2021-12-27 10:41:44 +0100

    updates

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -111,25 +111,27 @@ function Home(){
                 </div>
             </div>
             {/* Mobile-view */}
-            <div className = 'header-mobile rounded-3 text-white py-4'>
-                <div className = 'd-sm-flex d-md-flex d-lg-flex justify-content-sm-between justify-content-md-between justify-content-lg-between w-100'>
-                    <div className = ' head-content-mob'>
-                        <h3 className = 'text-heading-mob fw-bold text-white pt-2'>Automate Your Income.</h3>
-                        <p className = 'sub-text-mob text-white'>A platform for anyone looking to earn consistently<br/> off the forex
-                            market without losing control of their<br/> capital</p>
-                            <div className = 'row'>
-                                <div className  = 'col-6'>
-                                    <form action="https://link.us17.list-manage.com/subscribe/post?u=3d0c961e139280b56dff09d86&amp;id=ddbe36e928" method="post" class="row g-0">
-                                        <div class="d-grid wrapper-mob">
-                                        <input type="email"  value = {formValues.EMAIL} onChange ={handleChange} className="form-control field-mob rounded-start" name="EMAIL" placeholder="Enter your email" aria-label="Recipient's username" aria-describedby="button-addon2" id="inputEmail4-mob-home" required/>
-                                        <button class="btn rounded-end field-btn-mob" type="submit" value="Subscribe" name="subscribe" id="join-mob-home">Join the wait-list now</button>
-                                        </div>
-                                    </form>
+            <div className = 'container-fluid'>
+                <div className = 'header-mobile rounded-3 text-white py-4'>
+                    <div className = 'd-sm-flex d-md-flex d-lg-flex justify-content-sm-between justify-content-md-between justify-content-lg-between w-100'>
+                        <div className = ' head-content-mob'>
+                            <h3 className = 'text-heading-mob fw-bold text-white pt-2'>Automate Your Income.</h3>
+                            <p className = 'sub-text-mob text-white'>A platform for anyone looking to earn consistently<br/> off the forex
+                                market without losing control of their<br/> capital</p>
+                                <div className = 'row'>
+                                    <div className  = 'col-6'>
+                                        <form action="https://link.us17.list-manage.com/subscribe/post?u=3d0c961e139280b56dff09d86&amp;id=ddbe36e928" method="post" class="row g-0">
+                                            <div class="d-grid wrapper-mob">
+                                            <input type="email"  value = {formValues.EMAIL} onChange ={handleChange} className="form-control field-mob rounded-start" name="EMAIL" placeholder="Enter your email" aria-label="Recipient's username" aria-describedby="button-addon2" id="inputEmail4-mob-home" required/>
+                                            <button class="btn rounded-end field-btn-mob" type="submit" value="Subscribe" name="subscribe" id="join-mob-home">Join the wait-list now</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div className = ' col-6'>
+                                        <img src = {savings2} alt = 'savings' className = 'savings-mob pb-4'></img>
+                                    </div>
                                 </div>
-                                <div className = ' col-6'>
-                                    <img src = {savings2} alt = 'savings' className = 'savings-mob pb-4'></img>
-                                </div>
-                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
