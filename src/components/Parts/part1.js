593a53a3c286b901b52ddca681efc9e2e75941f6
Joycrown <olaniyanayoade@ymail.com>
2022-02-02 02:54:51 +0100

    push

--- src/components/Parts/part1.js
+++ src/components/Parts/part1.js
@@ -10,35 +10,35 @@ function One(){
                 <a class="navbar-brand" href="#">
                     <img src="" alt="LOGO" width="30" height="24"/>
                 </a>
-                <button class="navbar-toggler justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav nav-links nav-links-tab">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav  nav-links nav-links-tab">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-tab nav-link-mob" href="#">Packages</a>
+                            <a className="nav-link nav-link-tab nav-link-mob text-white" href="#">Packages</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-tab nav-link-mob">About</a>
+                            <a className="nav-link nav-link-tab nav-link-mob text-white">About</a>
                         </li>
                         <li className="nav-item nav-move">
-                            <a className="nav-link nav-link-tab nav-link-mob">Contact</a>
+                            <a className="nav-link nav-link-tab nav-link-mob text-white">Contact us</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-tab nav-link-mob">About</a>
+                            <a className="nav-link nav-link-tab nav-link-mob text-white">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link nav-link-tab nav-link-mob">SignIn</a>
+                            <a className="nav-link nav-link-tab nav-link-mob text-white">Sign In</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
         <div className= 'background-head vh-100' id = ' '>
-            <div className = 'heading heading-tab heading-mob' >
+            <div className = 'heading heading-tab'>
                 <h1 className = 'text-heading text-heading-tab text-heading-mob text-white'> Automate Your<br/>     Income</h1>
                 <h3 className = 'sub-text sub-text-tab sub-text-mob text-white pt-3'>Earn daily consistent profits with our<br/>
                  powerful copytrading software</h3>
@@ -46,8 +46,16 @@ function One(){
                     <button type="button"class="sub-btn sub-btn-tab sub-btn-mob btn btn-info text-white text-center">TRY IT FOR FREE</button>
                 </div>
             </div>
-            
-
+        </div>
+        <div className = 'background-head-mob vh-100' id = ' '>
+            <div className = 'heading-mob'>
+                <h1 className = 'text-heading-mob text-white'> Automate <br/> Your <br/>     Income</h1>
+                <h3 className = 'sub-text-mob text-white pt-3'>Earn daily consistent profits with our<br/>
+                 powerful copytrading software</h3>
+                 <div className = 'pt-3'>
+                    <button type="button"class="sub-btn-mob btn btn-info text-white text-center">TRY IT FOR FREE</button>
+                </div>
+            </div>
         </div>
     </section>
     );
